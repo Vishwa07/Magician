bf12800c6dcd4b73b4b250d120db3539119baac2
Vishwa07 <vishwanathm28@gmail.com>
2017-03-19 01:33:20 -0700

    Clean

--- HaystackImpl/src/HayStack_Directory.cpp
+++ HaystackImpl/src/HayStack_Directory.cpp
@@ -28,8 +28,6 @@ Directory* Directory::getInstance()
 string Directory::AssignTokenToWrite(string ImageID, unsigned long ImageSize)
 {
 	static int NextVolumeLocation = 0;
-	int start =0;
-	int end =NextVolumeLocation;
 	string result;
 	for(int i=NextVolumeLocation; i < TotalVolumes && AvailableVolumesToWrite[i] != 0; i++)
 	{
@@ -50,27 +48,6 @@ string Directory::AssignTokenToWrite(string ImageID, unsigned long ImageSize)
 			AvailableVolumesToWrite[i] = 0;
 		}
 
-	}
-	if(start!=end)
-	{
-		for(int i=start;i<end;i++)
-		{
-			int WritableVolume = AvailableVolumesToWrite[i];
-			if((SpaceMap[WritableVolume]+ImageSize) <= MaxSpaceForVolume)
-			{
-				SpaceMap[WritableVolume] += ImageSize;
-				NextVolumeLocation = (i+1)%(TotalVolumes);
-				StorageMap[ImageID] = WritableVolume;// what if image names repeat??
-				stringstream convert;
-				convert << WritableVolume;
-				result = (convert.str()+","+ImageID);
-				return result;
-			}
-			else
-			{
-				AvailableVolumesToWrite[i] = 0;
-			}
-		}
 	}
 
 	return "0";//No writable volumes found. All are full
